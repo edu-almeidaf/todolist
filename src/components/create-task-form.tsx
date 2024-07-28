5d0cbb8b4794a5d6984ddfb836c8e579f19d82b6
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-28 16:30:32 -0300

    feat: add validation in create new task form

--- src/components/create-task-form.tsx
+++ src/components/create-task-form.tsx
@@ -1,6 +1,6 @@
 import { PlusCircle } from 'phosphor-react'
 import styles from './create-task-form.module.css'
-import { ChangeEvent, useState, FormEvent } from 'react'
+import { ChangeEvent, useState, FormEvent, InvalidEvent } from 'react'
 
 interface CreateTaskFormProps {
   createNewTask: (task: string) => void
@@ -10,29 +10,32 @@ export function CreateTaskForm({ createNewTask }: CreateTaskFormProps) {
   const [taskContent, setTaskContent] = useState('')
 
   function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('')
     setTaskContent(event.target.value)
   }
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
 
-    if (!taskContent.trim()) {
-      return
-    }
-
     createNewTask(taskContent)
     setTaskContent('')
+  }
+
+  function handleNewTaskInvalid(event: InvalidEvent<HTMLInputElement>) {
+    event.target.setCustomValidity('Esse campo é obrigatório!')
   }
 
   return (
     <form className={styles.addTask} onSubmit={handleCreateNewTask}>
       <input
         type="text"
+        required
+        onInvalid={handleNewTaskInvalid}
         placeholder="Adicione uma nova tarefa"
         value={taskContent}
         onChange={handleNewTaskChange}
       />
-      <button type="submit">
+      <button type="submit" disabled={!taskContent.length}>
         Criar
         <PlusCircle weight="bold" />
       </button>
