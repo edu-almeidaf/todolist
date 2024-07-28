98b9353f19b6b63e5474b9b2a53cb82181c47c34
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-28 12:46:21 -0300

    feat: create form to create new task and tasks stats

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,43 @@
+import { PlusCircle } from 'phosphor-react'
 import styles from './app.module.css'
 import { Logo } from './assets/logo'
 
 export function App() {
   return (
-    <>
+    <div className={styles.container}>
       <header>
         <Logo />
         <h1 className={styles.title}>
           to<span>do</span>
         </h1>
       </header>
-    </>
+      <main>
+        <form className={styles.addTask}>
+          <input type="text" placeholder="Adicione uma nova tarefa" />
+          <button type="submit">
+            Criar
+            <PlusCircle weight="bold" />
+          </button>
+        </form>
+
+        <section className={styles.tasks}>
+          <div className={styles.tasksStats}>
+            <div>
+              <p>Tarefas criadas</p>
+              <div className={styles.counter}>
+                <span>0</span>
+              </div>
+            </div>
+
+            <div>
+              <p>Concluídas</p>
+              <div className={styles.counter}>
+                <span>0</span>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   )
 }
